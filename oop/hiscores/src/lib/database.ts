0f8b5cb6734571a8cb216817b7124a280539e4c1
Caspet04 <caspet@edu.umea.se>
2023-01-25 10:25:53 +0100

    fix: prisma exports not found error

--- oop/hiscores/src/lib/database.ts
+++ oop/hiscores/src/lib/database.ts
@@ -1,4 +1,4 @@
-import { PrismaClient as SqliteClient } from "../../prisma/generated/sqlite-client";
+import { PrismaClient as SqliteClient } from "@prisma/shared_client/sqlite_client";
 // import { PrismaClient as MongoDBClient } from "../../prisma/generated/mongodb-client";
 
 export const sqliteClient = new SqliteClient();
